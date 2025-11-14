71a769f3e95483af31fa040828865d496325a141
Christhian Montes Aguilar <39229428+stevenmontes@users.noreply.github.com>
2025-11-13 20:47:44 -0600

    Scene Manager // E14

--- 00_Globals/global_level_manager.gd
+++ 00_Globals/global_level_manager.gd
@@ -1,8 +1,37 @@
 extends Node
 
-var current_tilemap_bounds: Array[Vector2]
-
 signal tilemap_bounds_changed(bounds: Array[Vector2])
+signal level_loaded
+signal level_load_started
+
+var current_tilemap_bounds: Array[Vector2]
+var target_transition: String
+var position_offset: Vector2
+
+func _ready() -> void:
+	await get_tree().process_frame
+	level_loaded.emit()
+
+func load_new_level(level_path: String, new_target_transition: String, new_position_offset: Vector2) -> void:
+	get_tree().paused = true
+	target_transition = new_target_transition
+	position_offset = new_position_offset
+	
+	await SceneTransition.fade_out()
+	
+	level_load_started.emit()
+	
+	await get_tree().process_frame
+	
+	get_tree().change_scene_to_file(level_path)
+	
+	await SceneTransition.fade_in()
+	
+	get_tree().paused = false
+	
+	await get_tree().process_frame
+	
+	level_loaded.emit()
 
 func change_tilemap_bounds(bounds: Array[Vector2]) -> void:
 	current_tilemap_bounds = bounds
