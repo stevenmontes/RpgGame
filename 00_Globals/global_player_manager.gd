917c35e706f222b499e12b385a960d5c752cc0ba
Christhian Montes Aguilar <39229428+stevenmontes@users.noreply.github.com>
2025-11-18 22:30:37 -0600

    Basic Save & Load // E15

--- 00_Globals/global_player_manager.gd
+++ 00_Globals/global_player_manager.gd
@@ -13,6 +13,11 @@ func _ready() -> void:
 func add_player_instance() -> void:
 	player = PLAYER.instantiate()
 	add_child(player)
+	
+func set_health(hp: int, max_hp: int) -> void:
+	player.max_hp = max_hp
+	player.hp = hp
+	player.update_hp(0)
 
 func set_player_position(new_pos: Vector2) -> void:
 	player.global_position = new_pos
---
+++ 00_Globals/global_save_manager.gd
@@ -0,0 +1,65 @@
+extends Node
+
+const SAVE_PATH = "user://"
+
+signal game_loaded
+signal game_saved
+
+var current_save: Dictionary = {
+	scene_path = "",
+	player = {
+		hp = 1,
+		max_hp = 1,
+		pos_x = 1,
+		pos_y = 0
+	},
+	items = [],
+	persistance = [],
+	quests = []
+}
+
+func save_game() -> void: 
+	update_player_data()
+	update_scene_path()
+	var file := FileAccess.open(SAVE_PATH + 'save.sav', FileAccess.WRITE)
+	var save_json = JSON.stringify(current_save)
+	file.store_line(save_json)
+	game_saved.emit()
+	
+func load_game() -> void: 
+	var file := FileAccess.open(SAVE_PATH + 'save.sav', FileAccess.READ)
+	var json := JSON.new()
+	json.parse(file.get_line())
+	var save_dict : Dictionary = json.get_data() as Dictionary
+	current_save = save_dict
+	
+	LevelManager.load_new_level(current_save.scene_path, '', Vector2.ZERO)
+	
+	await LevelManager.level_load_started
+	
+	PlayerManager.set_player_position(Vector2(current_save.player.pos_x, current_save.player.pos_y))
+	PlayerManager.set_health(current_save.player.hp, current_save.player.max_hp)
+	
+	await LevelManager.level_loaded
+	
+	game_loaded.emit()
+	print('load game')
+	
+func update_player_data() -> void:
+	var current_player: Player = PlayerManager.player
+	current_save.player.hp = current_player.hp
+	current_save.player.max_hp = current_player.max_hp
+	current_save.player.pos_x = current_player.global_position.x
+	current_save.player.pos_y = current_player.global_position.y
+	
+func update_scene_path() -> void:
+	var path: String = ""
+	
+	for child in get_tree().root.get_children():
+		if child is Level:
+			path = child.scene_file_path
+			
+	current_save.scene_path = path
+	
+	
+	
