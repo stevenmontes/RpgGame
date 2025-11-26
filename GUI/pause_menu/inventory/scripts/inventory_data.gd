65e9f8536ea511677fdc45eaae57ea8ef6e60450
Christhian Montes Aguilar <39229428+stevenmontes@users.noreply.github.com>
2025-11-25 20:25:43 -0600

    Save & Load Items - Inventory (part III) // E18

--- GUI/pause_menu/inventory/scripts/inventory_data.gd
+++ GUI/pause_menu/inventory/scripts/inventory_data.gd
@@ -40,3 +40,41 @@ func add_item(item: ItemData, count: int = 1) -> bool:
 	print("inventory is full")
 	return false
  
+## Gather the inventory into an array
+func get_save_data() -> Array:
+	var item_save: Array = []
+	
+	for i in slots.size():
+		item_save.append(item_to_save(slots[i]))
+		
+	return item_save
+
+## Convert each inventory item into a dictionary
+func item_to_save(slot: SlotData) -> Dictionary:
+	var result = { item = "", quantity = 0 }
+	
+	if slot != null:
+		result.quantity = slot.quantity
+		if slot.item_data:
+			result.item = slot.item_data.resource_path
+	
+	return result
+
+func parse_save_data(save_data: Array) -> void:
+	var array_size = slots.size()
+	slots.clear()
+	slots.resize(array_size)
+	
+	for i in save_data.size():
+		slots[i] = item_from_save(save_data[i])
+		
+	connect_slots()
+	
+func item_from_save(save_object: Dictionary) -> SlotData:
+	if save_object.item == "":
+		return null
+		
+	var new_slot: SlotData = SlotData.new()
+	new_slot.item_data = load(save_object.item)
+	new_slot.quantity = int(save_object.quantity)	
+	return new_slot
